3a7767fc1c107a5835698f27cb362fc75e094be7
--replace-all <demola.hussainin@gmail.com>
2018-04-13 18:56:45 +0100

    refactor auto-generated migration file to ES6

---
+++ server/migrations/20180413172850-create-user.js
@@ -0,0 +1,29 @@
+
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    username: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('Users'),
+};
---
+++ server/models/user.js
@@ -0,0 +1,71 @@
+import crypto from 'crypto';
+
+const cipher = crypto.createCipher('aes192', 'my secrete');
+const decipher = crypto.createDecipher('aes192', 'my secrete');
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User', {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: 'username already exists',
+        },
+        validate: {
+          notEmpty: {
+            msg: 'field must not be empty',
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: 'field must not be empty',
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: 'email already exists',
+        },
+        validate: {
+          notEmpty: {
+            msg: 'field must not be empty',
+          },
+          isEmail: {
+            args: true,
+            msg: 'Please enter a valid email',
+          },
+        },
+      },
+    },
+    {
+      hooks: {
+        beforeCreate(user) {
+          let encrypted = cipher.update(user.password, 'utf8', 'hex');
+          encrypted += cipher.final('hex');
+          user.password = encrypted;
+        },
+        beforeUpdate(user) {
+          if (user.password) {
+            let encrypted = cipher.update(user.password, 'utf8', 'hex');
+            encrypted += cipher.final('hex');
+            user.password = encrypted;
+            user.updateAt = Date.now();
+          }
+        },
+      },
+    },
+  ); // end of define method
+  User.associate = () => {
+
+  };
+  return User;
+};
