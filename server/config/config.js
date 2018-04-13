9a6e9130e840563b1fe9a326682a0d28354095ae
--replace-all <demola.hussainin@gmail.com>
2018-04-13 18:27:04 +0100

    refactor code in config.js to conform to linting rules

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,25 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+export default {
+  development: {
+    username: 'postgres',
+    password: 'andelabootcamp24',
+    database: 'bakend-task',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    logging: false,
+    operatorsAliases: false,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
