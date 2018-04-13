e18984783fdec101c18711c273161a7534371124
--replace-all <demola.hussainin@gmail.com>
2018-04-13 17:58:31 +0100

    setup packages and linters for the app

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+
+const app = express();
+const port = 2121;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => {
+  res.status(200).send({ message: 'welcome to the backend task' });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    throw (err);
+  } else {
+    console.log(`Server is up and runnig on port ${port} ...`);
+  }
+});
+
+export default app;
